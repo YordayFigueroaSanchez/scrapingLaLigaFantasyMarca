a253aa2417ae27913477d20acfe02d2633b17003
yfigueroa <yfsanchez@gmail.com>
2020-02-10 09:31:14 -0300

    agregada jornada 22 y 23

--- src/scraping/ScrapingLaLigaFantasyMarca.java
+++ src/scraping/ScrapingLaLigaFantasyMarca.java
@@ -28,7 +28,7 @@ public class ScrapingLaLigaFantasyMarca {
 
 		String url = "https://www.laligafantasymarca.com/match/1/7/cd-leganes-vs-c-a-osasuna";
 
-		String file = "jornada21.html";
+		String file = "jornada23.html";
 		int contador = 0;
 
 		File input = new File("data/" + file);
